8a23f848ba7ffce3ad6c14329a4ad6ec9f6c9b43
Eleanor Ruth <ellie.ruth@me.com>
2022-12-01 15:35:11 -0700

    Add files for components

--- src/App/App.js
+++ src/App/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import React, { Component } from 'react'
-
+import logo from '../images/logo.png'
 
 class App extends Component {
   constructor() {
@@ -15,8 +15,34 @@ class App extends Component {
   }
 
   componentDidMount = () => {
-    fetch()
+    fetch("https://rancid-tomatillos.herokuapp.com/api/v2/movies")
+    .then(response => {
+      if (!response.ok) {
+        throw Error(response.text)
+      } else {
+        return response.json()
+      }
+    })
+    .then(data => {
+      this.setState({ movieData: data})
+    })
+    .catch(error => {
+      this.setState({ error: `something went wrong ${error}`})
+    })
   }
+
+  render() {
+    return(
+      <main>
+        {/* {this.state.error && <Error error={this.state.error}/>} */}
+        {/* <Nav /> */}
+        <img src={logo}/>
+        {/* <Banner />
+        <MovieContainer /> */}
+      </main>
+    )
+  }
+
 }
 
 export default App;
