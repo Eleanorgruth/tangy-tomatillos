e5f112479b0a432d3e8a2d6beb8cbddb20a88b8e
Eleanor Ruth <ellie.ruth@me.com>
2022-12-02 12:00:23 -0700

    Minor refactor

--- src/App/App.js
+++ src/App/App.js
@@ -14,7 +14,7 @@ class App extends Component {
       bannerMessage: '',
       error: '',
       currentView: '',
-      randomMovie: ''
+      randomMovie: {}
     }
   }
 
--- src/Banner/Banner.js
+++ src/Banner/Banner.js
@@ -2,15 +2,12 @@ import React from "react"
 import './Banner.css'
 
 const Banner = ({ randomMovie }) => {
-  console.log("LOOK HERE", typeof randomMovie)
-  console.log("RANDOMMOVIOE", randomMovie)
   const divStyle = {
     backgroundImage: 'url('+ randomMovie.backdrop_path + ')',
     height: '62vh',
     backgroundSize: 'cover',
     margin: '10%'
   }
-  // console.log("Banner, randomMovie", randomMovie)
   return (
     <section className="banner-container" style={divStyle}>
       <h2>All the best movies in one place</h2>
