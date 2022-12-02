4c74be59fb6833c8d567ae46d499ed7e4f5d1ffc
Tim T <tim.email.personal@gmail.com>
2022-12-01 14:22:09 -1000

    Add Nav with methods: handleChange, submitSearch, clearInputs

--- src/App/App.js
+++ src/App/App.js
@@ -1,8 +1,7 @@
 import './App.css';
 import React, { Component } from 'react'
 import logo from '../images/logo.png'
-
-// test
+import Nav from '../Nav/Nav'
 
 class App extends Component {
   constructor() {
@@ -49,7 +48,7 @@ class App extends Component {
     return(
       <main>
         {/* {this.state.error && <Error error={this.state.error}/>} */}
-        {/* <Nav /> */}
+        <Nav filterMovie={this.filterMovie}/>
         <img src={logo}/>
         {/* <Banner />
         <MovieContainer /> */}
