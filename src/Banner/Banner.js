fd9d1a4d0f4515fe5fe7ca9ce3eb8a5c2af0cc7e
Eleanor Ruth <ellie.ruth@me.com>
2022-12-01 20:50:49 -0700

    Fix the render for the randomMovie

--- src/Banner/Banner.js
+++ src/Banner/Banner.js
@@ -2,13 +2,20 @@ import React from "react"
 import './Banner.css'
 
 const Banner = ({ randomMovie }) => {
-  console.log("LOOK HERE", randomMovie[0].title)
+  console.log("LOOK HERE", typeof randomMovie)
+  console.log("RANDOMMOVIOE", randomMovie)
+  const divStyle = {
+    backgroundImage: 'url('+ randomMovie.backdrop_path + ')',
+    height: '62vh',
+    backgroundSize: 'cover',
+    margin: '10%'
+  }
   // console.log("Banner, randomMovie", randomMovie)
   return (
-    <section className="banner-container">
+    <section className="banner-container" style={divStyle}>
       <h2>All the best movies in one place</h2>
       <div className="banner-subcontainer">
-        <h3></h3>
+        <h3>{randomMovie.title}</h3>
         <button>View details</button>
       </div>
     </section>
