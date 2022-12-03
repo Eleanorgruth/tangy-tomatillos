1993ccf7b5ddcb36d3d688c889d7c36d9160c80d
Tim T <tim.email.personal@gmail.com>
2022-12-03 07:00:14 -1000

    Add conditionals for budgets and revenue at /bin/zsh

--- src/MovieDetailView/MovieDetailView.js
+++ src/MovieDetailView/MovieDetailView.js
@@ -2,7 +2,6 @@ import React from "react"
 import './MovieDetailView.css'
 
 const MovieDetailView = ({ selectedMovie }) => {
-  console.log(selectedMovie)
   return (
     <div>
       <div className="detail-backdrop-container">
@@ -12,23 +11,29 @@ const MovieDetailView = ({ selectedMovie }) => {
 
       <div className="detail-container">
         <div className="detail-left-container">
-         
-          <img src={selectedMovie.poster_path} className="detail-poster"/>
+
+          <img src={selectedMovie.poster_path} className="detail-poster" />
         </div>
 
         <div className="detail-right-container">
-        <h1 className="detail-title">{selectedMovie.title}</h1>
-        <h3 className="detail-tagline">{selectedMovie.tagline}</h3>
-        <p className="overview">{selectedMovie.overview}</p>
-        <ul>
-          <li>Rating: {selectedMovie.average_rating.toFixed(1)}/10</li>
-          <li>Budget: ${selectedMovie.budget}</li>
-          <li>Revenue: ${selectedMovie.revenue}</li>
-          <li>Runtime: {selectedMovie.runtime}</li>
-          <li>Release Date: {selectedMovie.release_date}</li>
-          <li>Tags: {selectedMovie.genres.toString().split(',').join(', ')}</li>
-        </ul>
-      </div>
+          <h1 className="detail-title">{selectedMovie.title}</h1>
+          <h3 className="detail-tagline">{selectedMovie.tagline}</h3>
+          <p className="overview">{selectedMovie.overview}</p>
+          <ul>
+            <li>Rating: {selectedMovie.average_rating.toFixed(1)}/10</li>
+            { selectedMovie.budget
+              ? <li>Budget: ${selectedMovie.budget}</li>
+              : <li>Budget: not available</li>
+            }
+            { selectedMovie.revenue
+              ? <li>Revenue: ${selectedMovie.revenue}</li>
+              : <li>Revenue: not available</li>
+            }
+            <li>Runtime: {selectedMovie.runtime}</li>
+            <li>Release Date: {selectedMovie.release_date}</li>
+            <li>Tags: {selectedMovie.genres.toString().split(',').join(', ')}</li>
+          </ul>
+        </div>
       </div>
 
     </div>
