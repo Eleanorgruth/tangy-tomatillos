8698d94c70fb6a11936c094b4f0f9c049d16c6c0
Eleanor Ruth <ellie.ruth@me.com>
2022-12-06 18:29:03 -0700

    Add Banner to return

--- src/MovieDetailView/MovieDetailView.js
+++ src/MovieDetailView/MovieDetailView.js
@@ -23,6 +23,7 @@ class MovieDetailView extends Component {
       })
       .then(data => {
         this.setState({ selectedMovie: data.movie })
+        console.log("DATA", data.movie)
       })
       .catch(error => {
         this.setState({ error: `something went wrong ${error}` })
@@ -31,38 +32,39 @@ class MovieDetailView extends Component {
   render() {
     return (
       <div>
-        <p>test {this.state.selectedMovie.title}</p>
-        {/* <div className="detail-backdrop-container">
-        <img src={movieData.backdrop_path} className='poster-styling' />
+      <div className="detail-backdrop-container">
+        <img src={this.state.selectedMovie.backdrop_path} className='poster-styling' />
       </div>
       <div className="detail-container">
         <div className="detail-left-container">
-          <img src={movieData.poster_path} className="detail-poster"/>
+          <img src={this.state.selectedMovie.poster_path} className="detail-poster"/>
         </div>
         <div className="detail-right-container">
-          <h1 className="detail-title">{movieData.title}</h1>
-          <h3 className="detail-tagline">{movieData.tagline}</h3>
-          <p className="overview">{movieData.overview}</p>
+          <h1 className="detail-title">{this.state.selectedMovie.title}</h1>
+          <h3 className="detail-tagline">{this.state.selectedMovie.tagline}</h3>
+          <p className="overview">{this.state.selectedMovie.overview}</p>
           <ul>
-            <li>Rating: {movieData.average_rating.toFixed(1)}/10 {` `}
+            {/* <li>Rating: {this.state.selectedMovie.average_rating.toFixed(1)}/10 {` `} */}
+            <li>Rating: {this.state.selectedMovie.average_rating}/10 {` `}
             <img className='detail-icon-styling'
               src={icon}
               />
             </li>
-            { movieData.props.budget
-              ? <li>Budget: ${movieData.budget.toString().slice(movieData.budget.length, -6)}M</li>
+            { this.state.selectedMovie.budget
+              ? <li>Budget: ${this.state.selectedMovie.budget.toString().slice(this.state.selectedMovie.budget.length, -6)}M</li>
               : <li>Budget: not available</li>
             }
-            { movieData.revenue
-              ? <li>Revenue: ${movieData.revenue.toString().slice(movieData.revenue.length, -6)}M</li>
+            { this.state.selectedMovie.revenue
+              ? <li>Revenue: ${this.state.selectedMovie.revenue.toString().slice(this.state.selectedMovie.revenue.length, -6)}M</li>
               : <li>Revenue: not available</li>
             }
-            <li>Runtime: {movieData.runtime} minutes</li>
-            <li>Release Date: {new Date(movieData.release_date).toLocaleString('default', { month: 'long' })} {new Date(movieData.release_date).getDay()}, {new Date(movieData.release_date).getFullYear()}</li>
-            <li>Genres: {movieData.genres.toString().split(',').join(', ')}</li>
+            <li>Runtime: {this.state.selectedMovie.runtime} minutes</li>
+            <li>Release Date: {new Date(this.state.selectedMovie.release_date).toLocaleString('default', { month: 'long' })} {new Date(this.state.selectedMovie.release_date).getDay()}, {new Date(this.state.selectedMovie.release_date).getFullYear()}</li>
+            <li>Genres: {this.state.selectedMovie.genres}</li>
+            {/* <li>Genres: {this.state.selectedMovie.genres.toString().split(',').join(', ')}</li> */}
           </ul>
         </div>
-      </div> */}
+      </div>
       </div>
     )
   }
