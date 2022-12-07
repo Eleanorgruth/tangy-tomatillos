bbc9b30a0eac2d1e71545b874886a26bfc827684
Eleanor Ruth <ellie.ruth@me.com>
2022-12-06 18:35:12 -0700

    Move Link to just the Poster image

--- src/MoviePosterCard/MoviePosterCard.js
+++ src/MoviePosterCard/MoviePosterCard.js
@@ -5,9 +5,10 @@ import { Link } from 'react-router-dom'
 
 const MoviePosterCard = ({ title, releaseDate, posterPath, averageRating, id }) => {
   return (
-    <Link to={`/${id}`}>
-      <section className='movie-card-styling' key={id}>
-        <img className='movie-image-styling' src={posterPath} alt={title} />
+    <section className='movie-card-styling' key={id}>
+        <Link to={`/${id}`}>
+          <img className='movie-image-styling' src={posterPath} alt={title} />
+        </Link>
         <p>Rating {averageRating.toFixed(1)}/10
           <img
             className='icon-styling'
@@ -16,7 +17,6 @@ const MoviePosterCard = ({ title, releaseDate, posterPath, averageRating, id }) 
         </p>
         <p className='released-styling'>Released {releaseDate.slice(0, 4)}</p>
       </section>
-    </Link>
 
   )
 }
